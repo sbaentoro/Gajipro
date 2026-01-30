e4fc2adb233d02d42c3a17c58d3aa20f7e5c1768
sbaentoro <128255282+sbaentoro@users.noreply.github.com>
2026-01-30 12:18:50 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'gajipro-v10-offline';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache if found, otherwise fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
